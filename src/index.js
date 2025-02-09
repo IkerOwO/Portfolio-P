3841da715f354d8c1fd3c098ffe0f51bdcb8c2de
Iker <162556194+IkerOwO@users.noreply.github.com>
2025-02-09 15:08:08 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 // Btn Abajo
 document.getElementById('scrollButton').addEventListener('click', function() {
-    document.getElementById('sobre').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('adios').scrollIntoView({ behavior: 'smooth' });
 });
 
 // Btn Arriba
 document.getElementById('scrollupButton').addEventListener('click', function() {
-    document.getElementById('git').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('hola').scrollIntoView({ behavior: 'smooth' });
 });
 
 
@@ -18,4 +18,16 @@ window.addEventListener('load', function() {
 window.addEventListener('load', function() {
     const animatedWord2 = document.getElementById('animatedWord2');
     animatedWord2.classList.add('visible'); // Add the 'visible' class when the page loads
+});
+
+//Boton Secret
+const btnSend = document.getElementById('send');
+const text = document.getElementById('secret');
+const secretText = document.getElementById('secretText');
+
+btnSend.addEventListener('click', () => {
+    const textInput = text.value; 
+    if (textInput === "Secret" || textInput === "secret") {
+        secretText.classList.remove('hidden'); 
+    }
 });
